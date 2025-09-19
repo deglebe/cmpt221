b023eeea1dfdd9cf655c6f80fc722c2ded5eed33
deglebe <tdb@tdbio.me>
2025-09-19 12:34:12 -0400

    completed lab 3

--- labs/lab-3/db/schema/professor.py
+++ labs/lab-3/db/schema/professor.py
@@ -4,17 +4,23 @@ from db.server import db
 class Professor(db.Model):
     __tablename__ = 'Professors'
     ProfessorID = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    ProfessorFN = db.Column(db.String(40))
+    ProfessorLN = db.Column(db.String(40))
+    ProfessorEmail = db.Column(db.String(40))
 
     # create relationship with courses table. assoc table name = ProfessorCourse
     course = db.relationship('Courses', secondary = 'ProfessorCourse', back_populates = 'Professors')
-    def __init__(self):
-        # remove pass and then initialize attributes
-        pass
+
+    def __init__(self, first_name, last_name, email):
+        self.ProfessorFN = first_name
+        self.ProfessorLN = last_name
+        self.ProfessorEmail = email
 
     def __repr__(self):
-        # add text to the f-string
         return f"""
-
+            PROFESSOR ID:   {self.ProfessorID}
+            PROFESSOR NAME: {self.ProfessorFN} {self.ProfessorLN},
+            EMAIL:          {self.ProfessorEmail}
         """
     
     def __repr__(self):
